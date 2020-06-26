c7ea78509bac17c1fdebf16f6432b3bf97fcf51d
Chris Gray <cgray2792@gmail.com>
2020-06-26 12:05:45 +0100

    Add port 80 and 32768-61000, split rules out into resources. Co-authored-by: Matt Tortolani <matt@doodlemoonch.com>

--- assessments/cloud-platform-engineer/iac/main.tf
+++ assessments/cloud-platform-engineer/iac/main.tf
@@ -1,28 +1,36 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_security_group" "bymiles-security-group" {
   description = "A security group"
-  vpc_id      = "vpc-bb571bd3"
+  vpc_id      = "${aws_vpc.main.id}"
   name        = "bymiles-security-group"
+}
 
-  ingress {
-    protocol  = "tcp"
-    from_port = 32768
-    to_port   = 61000
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "http" {
+  security_group_id = "${aws_security_group.bymiles-security-group.id}"
+  type              = "ingress"
+  protocol          = "TCP"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "ingress" {
   security_group_id = "${aws_security_group.bymiles-security-group.id}"
   type              = "ingress"
   protocol          = "TCP"
-  from_port         = 80
-  to_port           = 80
+  from_port         = 32768
+  to_port           = 61000
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "egress" {
+  security_group_id = "${aws_security_group.bymiles-security-group.id}"
+  type              = "egress"
+  protocol          = "-1"
+  from_port         = 0
+  to_port           = 0
   cidr_blocks       = ["0.0.0.0/0"]
 }
